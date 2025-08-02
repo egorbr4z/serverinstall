962a7ee2b9ed09cffee9b5f9a251af0ab0665850
egorbr4z <briz667.1@gmail.com>
2025-08-02 17:12:25 +0000

    Version Two

--- debian.py
+++ debian.py
@@ -59,4 +59,23 @@ os.system(command)
 input("Press Enter to install and enable ufw")
 print("Enable ufw")
 
+os.system("sudo ufw allow 3306/tcp"
+"sudo ufw enable"
+)
 
+print("Add a 80 and 443 port to ufw?")
+ufw_port = input("Answer(yes or no): ")
+
+if ufw_port = y or yes or Yes:
+       os.system("sudo ufw allow 80/tcp" && "sudo ufw allow 443/tcp")
+else:
+       print("Skiped")	
+
+# Checkout
+print("Make a checkout of Apache?")
+checkout_answer = input("Answer(yes or no): ")
+if checkout_answer = y or yes or Yes:
+      print("Apache Service")
+      os.system("curl -I localhost")
+else:
+      print("Finish!")
